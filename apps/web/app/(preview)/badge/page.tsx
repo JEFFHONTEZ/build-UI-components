3f6c8c9f0e52abc73a568c8e0d3cf414c3ffef26
Jeff Kamau <jeffkamau8501@gmail.com>
2026-07-16 14:14:21 +0300

    create badge components and it's valiants

--- apps/web/app/(preview)/badge/page.tsx
+++ apps/web/app/(preview)/badge/page.tsx
@@ -1,10 +1,18 @@
 import { Badge } from "@workspace/ui/components/badge"
+import { cn } from "../../../../../packages/ui/src/lib/utils";
 
 export default function BadgePreview() {
   return (
     <div className="p-10">
-      <h1 className="mb-6 text-lg font-semibold">Badge</h1>
+      <h1 className={cn('mb-6', 'text-lg', 'font-semibold')}>Badge</h1>
+      <div className={cn('flex', 'flex-wrap', 'gap-4')}>
       <Badge>Example Badge</Badge>
+      <Badge variant="secondary">Secondary Badge</Badge>
+      <Badge variant="destructive">Destructive Badge</Badge>
+      <Badge variant="outline">Outline Badge</Badge>
+      <Badge variant="ghost">Ghost Badge</Badge>
+      <Badge variant="link">Link Badge</Badge>
+      </div>
     </div>
   )
 }
