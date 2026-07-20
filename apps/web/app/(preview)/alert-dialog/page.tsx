84c3a2971749b7d661d075aec836ddf0ae76e081
Jeff Kamau <jeffkamau8501@gmail.com>
2026-07-20 08:45:28 +0300

    edited some components

--- apps/web/app/(preview)/alert-dialog/page.tsx
+++ apps/web/app/(preview)/alert-dialog/page.tsx
@@ -1,10 +1,11 @@
-import { AlertDialog } from "@workspace/ui/components/alert-dialog"
+import { AlertDialog, AlertDialogTrigger } from "@workspace/ui/components/alert-dialog"
 
 export default function AlertDialogPreview() {
   return (
     <div className="p-10">
       <h1 className="mb-6 text-lg font-semibold">Alert Dialog</h1>
       <AlertDialog>Example AlertDialog</AlertDialog>
+      <AlertDialogTrigger> Example of a AlertDialogTrigger </AlertDialogTrigger>
     </div>
   )
 }
--- apps/web/app/(preview)/card/page.tsx
+++ apps/web/app/(preview)/card/page.tsx
@@ -1,10 +1,13 @@
-import { Card } from "@workspace/ui/components/card"
+import { Card, CardTitle } from "@workspace/ui/components/card"
 
 export default function CardPreview() {
-  return (
-    <div className="p-10">
-      <h1 className="mb-6 text-lg font-semibold">Card</h1>
-      <Card>Example Card</Card>
-    </div>
-  )
+    return (
+        <div className="p-10">
+              <h1 className="mb-6 text-lg font-semibold">Card</h1>
+              <div className="flex flex-wrap gap-4">
+                <Card>Default</Card>
+                </div>
+              </div>
+
+    )
 }
